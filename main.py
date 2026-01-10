f224a84f3e569270d550ed066b1a096a2ee14c13
NaumSimanduyev <simanduyevnaum@gmail.com>
2026-01-10 18:02:22 +0400

    bug-fixes

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def display_tasks():
     try:
         with open('data.json', 'r') as f:
             data = json.load(f)
+
         print('Your current tasks:')
+
         if len(data.keys()) == 0:
             print('No current tasks')
         else:
@@ -12,30 +14,38 @@ def display_tasks():
                 print(i)
                 print('\t', end='')
                 print(*data[i], sep=f'\n\t')
+
     except json.decoder.JSONDecodeError:
         print('No current tasks.')
 
 def new_task():
     date = datetime.date.today().strftime("%y:%m:%d")
+
     try:
         with open('data.json', 'r') as f:
             data = json.load(f)
-    except json.decoder.JSONDecodeError:
+    except:
         data = {}
+
     if date not in data.keys():
         data[date] = list()
+
     data[date].append(input('Write a task: '))
+
     with open('data.json', 'w') as f:
         json.dump(data, f, indent=4)
+
     display_tasks()
 
-def remove_tasks():
+def remove_task():
     try:
         with open('data.json', 'r') as f:
             data = json.load(f)
-        while True:
+
+        display_tasks()
+        while len(data) != 0:
             time.sleep(1)
-            display_tasks()
+            
 
             date = input('choose a date: ').strip()
             if date in data.keys():
@@ -48,7 +58,6 @@ def remove_tasks():
                     print('Please enter a valid task number to delete.')
                     continue
                 
-
                 if data[date] == []:
                     del data[date]
 
@@ -59,20 +68,16 @@ def remove_tasks():
                 break
             else:
                 print('Please enter a valid date.')
+
     except json.decoder.JSONDecodeError:
         print('There are no tasks to delete')
 
 print('Welcome to the To Do app.')
 while True:
     time.sleep(1)
-    action = input('What do you want to do?\n1) Add a new task\n2) Watch current tasks\n3) Remove a task\n4) Quit the program\n')
-    if action == '1':
-        new_task()
-    elif action == '2':
-        display_tasks()
-    elif action == '3':
-        remove_tasks()
-    elif action == '4':
-        break
+    actions = {'1': new_task, '2': display_tasks, '3': remove_task}
+    action = input('What do you want to do?\n1) Add a new task\n2) Watch current tasks\n3) Remove a task\n').strip()
+    if action in actions.keys():
+        actions[action]()
     else:
         print('Please enter a valid command.')
